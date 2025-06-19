2714f3df77d38b1f2ad8336f096ea339e20c26b2
William Horn <wbhorn@alaska.edu>
2025-06-19 15:26:08 -0800

    Rough get_frames function

--- src/asf_enumeration/aria_s1_gunw.py
+++ src/asf_enumeration/aria_s1_gunw.py
@@ -1,4 +1,6 @@
 import datetime
+import importlib.resources
+import json
 from dataclasses import dataclass
 
 
@@ -7,10 +9,40 @@ class AriaFrame:
     frame_id: int
     path: int
     flight_direction: str
+    polygon: str
+
+    def does_intersect(self, polygon) -> bool:
+        # TODO: check intersection
+        return True
 
 
 def get_frames(polygon: str = None, flight_direction: str = None, path: int = None) -> list[AriaFrame]:
-    pass
+    flight_directions = ['ascending', 'descending'] if flight_direction is None else [flight_direction]
+    aria_frames = []
+
+    for direction in flight_directions:
+        with importlib.resources.path('asf_enumeration.aria_frames', f'{direction.lower()}.geojson') as frame_file:
+            frames = json.loads(frame_file.read_text())
+
+        for frame in frames['features']:
+            props = frame['properties']
+
+            if path and path != props['path']:
+                continue
+
+            aria_frame = AriaFrame(
+                frame_id=props['id'],
+                path=props['path'],
+                flight_direction=props['dir'],
+                polygon=frame['geometry']['type']
+            )
+
+            if polygon and not aria_frame.does_intersects(polygon):
+                continue
+
+            aria_frames.append(aria_frame)
+
+    return aria_frames
 
 
 def get_stack(frame_id: int) -> list[datetime.date]:
--- tests/test_aria_s1_gunw.py
+++ tests/test_aria_s1_gunw.py
@@ -2,4 +2,20 @@ from asf_enumeration import aria_s1_gunw
 
 
 def test_all_get_frames():
-    assert aria_s1_gunw.get_frames()
+    frames = aria_s1_gunw.get_frames()
+
+    assert len(frames) == 27563
+
+
+def test_get_frames_by_path():
+    frames = aria_s1_gunw.get_frames(path=100)
+
+    assert all([frame.path == 100 for frame in frames])
+
+
+def test_get_frames_by_flight_direction():
+    ascending = aria_s1_gunw.get_frames(flight_direction='ASCENDING')
+    assert all([frame.flight_direction == 'ASCENDING' for frame in ascending])
+
+    descending = aria_s1_gunw.get_frames(flight_direction='DESCENDING')
+    assert all([frame.flight_direction == 'DESCENDING' for frame in descending])
